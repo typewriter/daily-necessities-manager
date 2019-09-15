f0273fd1bb5ba092151bdb7082d79082cf5a6fa8
typewriter <takuyaxp@gmail.com>
2019-09-15 22:48:35 +0900

    Implement FirebaseUI

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,19 +1,41 @@
 <template>
-  <div class="login">
-    <button v-on:click="authentication">認証</button>
+  <div class="login text-left">
+    <b-container>
+      <div id="firebaseui-container"></div>
+    </b-container>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import firebase from "firebase";
+import firebase from "firebase/app";
+import "firebase/auth";
+import firebaseui from "firebaseui-ja";
+import "firebaseui/dist/firebaseui.css";
 
 @Component({
-  methods: {
-    authentication: function() {
-      var provider = new firebase.auth.GoogleAuthProvider();
-      firebase.auth().signInWithRedirect(provider);
-    }
+  mounted: function() {
+    var uiConfig = {
+      signInSuccessUrl: './',
+      signInOptions: [
+        // Leave the lines as is for the providers you want to offer your users.
+        firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+        firebase.auth.EmailAuthProvider.PROVIDER_ID,
+      ],
+      // tosUrl and privacyPolicyUrl accept either url string or a callback
+      // function.
+      // Terms of service url/callback.
+      tosUrl: './',
+      // Privacy policy url/callback.
+      privacyPolicyUrl: function() {
+        window.location.assign('./privacy_policy');
+      }
+    };
+
+    // Initialize the FirebaseUI Widget using Firebase.
+    var ui = new firebaseui.auth.AuthUI(firebase.auth());
+    // The start method will wait until the DOM is loaded.
+    ui.start('#firebaseui-container', uiConfig);
   }
 })
 export default class Login extends Vue {}
